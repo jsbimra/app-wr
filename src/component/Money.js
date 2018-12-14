da252f0ea6218a8ca2a36ad8d03526cad9b62db0
jsbimra <jsbimra@users.noreply.github.com>
2018-12-14 14:04:44 +0530

    semver 1.1.0

--- src/component/Money.js
+++ src/component/Money.js
@@ -8,8 +8,8 @@ function getRandomTop(max) {
 }
 const MoneyTray = (props) => {
     console.log(props);
-    const coin = props.coins ? props.coins.map((coin, idx) => (<span key={idx} onClick={(e)=> { props.onCoinClick(coin, e) } } className="coin">{coin}</span>)) : (<span className="no-coins">No coins</span>);
-    const bill = props.coins ? props.bills.map((bill, idx) => (<span key={idx} onClick={(e)=> { props.onBillClick(bill, e) } } className="bill">{bill}</span>)) : (<span className="no-bills">No coins</span>);
+    const coin = props.coins ? props.coins.map((coin, idx) => (<span key={idx} onClick={(e) => { props.onCoinClick(coin, e) }} className="coin">{coin}</span>)) : (<span className="no-coins">No coins</span>);
+    const bill = props.coins ? props.bills.map((bill, idx) => (<span key={idx} onClick={(e) => { props.onBillClick(bill, e) }} className="bill">{bill}</span>)) : (<span className="no-bills">No coins</span>);
     return (
         <div className="money-tray">
             <div className="coins-wrapper">
@@ -24,15 +24,22 @@ const MoneyTray = (props) => {
     );
 }
 
-
 const MoneyPot = (props) => {
+    console.log(props);
     return (
-        <div className={`money-pot ${props.revealPotFlag ? 'reveal-pot' : ''}`}></div>
+        <div className={`money-pot ${props.revealPotFlag ? 'reveal-pot' : ''} ${props.breakedFlag ? 'reveal-pot breaked' : ''} `}></div>
     )
 }
 
-
 class Money extends Component {
+    moneyState = {
+        breakedFlag: false,
+        revealPotChecked: false,
+        coins: [1, 2, 5, 10, 20],
+        bills: [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000],
+        collectMoney: [],
+        totalSavings: 0
+    }
     constructor(props) {
         super(props);
         this.state = this.moneyState;
@@ -40,82 +47,159 @@ class Money extends Component {
         this.onBillClick = this.onBillClick.bind(this);
         this.onRevealPotHandler = this.onRevealPotHandler.bind(this);
         this.breakPotKnowSavings = this.breakPotKnowSavings.bind(this);
+        this.onReBuildSavings = this.onReBuildSavings.bind(this);
     }
     componentDidMount() {
         this.setState({
             coins: this.state.coins.reverse()
         })
     }
-    
-    moneyState = {
-        revealPotChecked: false,
-        coins: [1, 2, 5, 10, 20],
-        bills: [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000],
-        totalSavings: []
+    componentWillUnMount() {
+        
     }
-    breakPotKnowSavings() {
-        let totalSavings = this.state.totalSavings;
-        if(totalSavings.length) {
-            totalSavings = totalSavings.reduce((acc, curval) => acc + curval);
-            console.log(this.state.totalSavings);
+    calculateTotalSavings(collectMoney) {
+        console.clear();
+        console.log(collectMoney);
+
+        if (collectMoney.length) {
+            let totalSavings = collectMoney.reduce((acc, curval) => acc + curval);
+            console.log(totalSavings);
             console.log('Your total saving in the pot is: ', totalSavings);
-            
+
             this.setState({
-                revealPotChecked: true
+                totalSavings
             });
 
-            $('.money-pot').css({width: '100%' });
-            $('.money-pot .coin, .money-pot .bill').css({position: 'relative', bottom: 0, top: 'auto'});
+            return totalSavings;
         }
     }
-    addToPot(money, event){
+    breakPotKnowSavings() {
+
+        this.calculateTotalSavings(this.state.collectMoney);
+
+        this.setState({
+            revealPotChecked: true,
+            breakedFlag: true
+        });
+    }
+    addToPot(money, event) {
         const $elCoin = $(event.target);
         let $cloneCoinEl;
 
         console.log('Clicked whichCoin ', money);
-        
-        $elCoin.css({top: `${getRandomTop(320)}px`, left: `${getRandomTop(240)}px`});
+
+        $elCoin.css({ top: `${getRandomTop(320)}px`, left: `${getRandomTop(240)}px` });
         $cloneCoinEl = $elCoin.clone(false);
         $('.money-pot').append($cloneCoinEl);
 
-        let totalSavings = [...this.state.totalSavings, money];
-        console.log('adding money to savings ', totalSavings);
+        let collectMoney = [...this.state.collectMoney, money];
+        console.log('adding money to savings ', collectMoney);
+
+        $cloneCoinEl.css({ transform: `rotateZ( ${getRandomTop(360)}deg) ` });
+
         this.setState({
-            totalSavings
+            collectMoney
         });
     }
     onCoinClick(which, event) {
-        if(which) {
+        if (which) {
             // console.log('Clicked which coin ', which);
             this.addToPot(which, event);
         }
     }
     onBillClick(which, event) {
         console.log(event);
-        if(which) {
+        if (which) {
             // console.log('Clicked which bill ', which);
             this.addToPot(which, event);
         }
     }
-    onRevealPotHandler(){
+    onRevealPotHandler() {
         this.setState({
             revealPotChecked: !this.state.revealPotChecked
         });
     }
+    onReBuildSavings(){
+        this.setState({
+            breakedFlag: false,
+            revealPotChecked: false,
+            collectMoney: [],
+            totalSavings: 0
+        });
+        $('.money-pot').html('');
+    }
     render() {
+        const RevealPot = (props) => {
+            if (this.state.collectMoney.length) {
+                return (
+                    <label className="float-left">
+                        <input type="checkbox" checked={this.state.revealPotChecked}
+                            name="chkRevealPot"
+                            value={this.state.revealPotChecked}
+                            onChange={this.onRevealPotHandler} /> Reveal Pot</label>)
+            }
+            return '';
+        }
+        const BreakButton = () => {
+            if (!this.state.breakedFlag) {
+                if (this.state.collectMoney.length) {
+                    return (
+                        <label className="float-right">You want to break your pot ?  <button className=" btn-small" onClick={this.breakPotKnowSavings}>Break Now</button>
+                        </label>)
+                }
+            }
+            return '';
+        }
+
+        const TotalSavings = () => {
+            if (this.state.totalSavings) {
+                return (
+                    <div className="total-savings text-center">Your total saving till now: {this.state.totalSavings}</div>
+                )
+            }
+            return '';
+        }
+        
+        const ReBuildSavings = () => {
+            if (this.state.totalSavings) {
+                return (
+                    <div className="rebuild-savings text-center">
+                        <button className=" btn-small" onClick={this.onReBuildSavings}>Rebuild Savings from Zero</button>
+                    </div>
+                )
+            }
+            return '';
+        }
+        
+        const layTray = () => {
+            if (!this.state.breakedFlag) {
+                return (
+                    <div className="clearfix">
+                        <MoneyTray
+                            coins={this.state.coins}
+                            bills={this.state.bills}
+                            onCoinClick={this.onCoinClick}
+                            onBillClick={this.onBillClick} />
+
+                        <RevealPot />
+                        <BreakButton />
+                    </div>
+                )
+            } else {
+                return (
+                    <div className="clearfix">
+                        <TotalSavings />
+                    </div>
+                )
+            }
+        }
+
         return (
             <div className="money-container">
-                <h1>Money</h1>
-                <MoneyTray coins={this.state.coins} bills={this.state.bills} 
-                    onCoinClick={this.onCoinClick }
-                    onBillClick={this.onBillClick } />
-
-                <label className="float-left"><input type="checkbox" name="chkRevealPot" value={this.state.revealPotChecked} onChange={this.onRevealPotHandler} /> Reveal Pot</label>
-                {this.state.totalSavings.length ? (<label className="float-right">You want to break your pot now ? <button className=" btn-small" onClick={this.breakPotKnowSavings}>Break the pot</button></label>) : ''}
-                
-                <div className="clearfix"></div>
-                
-                <MoneyPot takeMoney="0" revealPotFlag={this.state.revealPotChecked} />
+                <h1>Your daily money saver</h1>
+                {layTray()}
+                <MoneyPot takeMoney="0" revealPotFlag={this.state.revealPotChecked} breakedFlag={this.state.breakedFlag} />
+                <ReBuildSavings />
             </div>
         )
     }
